b525198910139e884f1c43dd0d40ffd668b91902
JeyaGuru <37777472+JeyaGuru@users.noreply.github.com>
2018-03-25 22:37:04 +0530

    Update stringCharSort.java

--- stringCharSort.java
+++ stringCharSort.java
@@ -1,60 +1,33 @@
-
-
 import java.util.*;
-
-
-
 class stringCharSort
   {
-	
-
 public static void main (String[] args)
-
      {		
 	Scanner s=new Scanner(System.in);
-
 	String st=s.next();
-		
-
-		
+	
 	int ar[]=new int[st.length()];
 	
-
-
 	for(int i=0;i<ar.length;i++)
-   //string to int array	
-	{ar[i]=st.charAt(i);}
-
-                             		     
+           {ar[i]=st.charAt(i);}    //string to int array              		     
+	  	int i=0;
 	
-	  	int i=0;	    
 	 for(int j=i+1;j<ar.length;j++)
-
  	    {   
-		   
-     		if(ar[i]>ar[j])
-	        //sort array(ascending)	
-        	{
-		 
-         	int t=ar[i];
-		          
+     		if(ar[i]>ar[j])    //sort array(ascending)	
+        	{ 
+         	int t=ar[i];         
          	ar[i]=ar[j];
-		
           	ar[j]=t;
-		    
                 }
 		
-      
-             if(i<ar.length && j==ar.length-1)
-             { 
-              System.out.print((char)ar[i]);
-       //re call loop & print element
+               if(i<ar.length && j==ar.length-1)      //re call loop & print element
+               { 
+               System.out.print((char)ar[i]);
                i++;
                j=i-1;
-             }
-         
-            }
-
+               }
+           }
      }
 }
  		
