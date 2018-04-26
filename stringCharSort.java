78d62dec608cb81a7c6da0eabce4e49fac6709a7
JeyaGuru <37777472+JeyaGuru@users.noreply.github.com>
2018-04-27 01:35:28 +0530

    Update stringCharSort.java

--- stringCharSort.java
+++ stringCharSort.java
@@ -1,3 +1,7 @@
+------------------------
+without using functions:
+------------------------
+
 import java.util.*;
 class stringCharSort
   {
@@ -31,4 +35,52 @@ public static void main (String[] args)
      }
 }
  		
-		
+----------------
+using functions: 
+----------------
+	
+import java.util.*;
+class stringCharSort
+  {
+	
+      int[] stringtoIntArray(String st)
+      {
+          	int ar[]=new int[st.length()];
+	    for(int i=0;i<ar.length;i++)
+            ar[i]=st.charAt(i);
+            return ar;
+      }
+	
+      void sort(int ar[])
+      {
+	    for(int i=0;i<ar.length;i++){
+	    for(int j=i+1;j<ar.length;j++)
+ 	       {   
+     		if(ar[i]>ar[j])   	
+        	   { 
+         	    int t=ar[i];         
+         	    ar[i]=ar[j];
+          	    ar[j]=t;
+               }
+ 	       }
+        }
+      }
+	
+      void print(int ar[])
+      {
+          for(int i=0;i<ar.length;i++)
+          System.out.print((char)ar[i]);
+      }
+	
+      public static void main (String[] args)
+     {		
+	Scanner s=new Scanner(System.in);
+	stringCharSort ob=new stringCharSort();
+	String st=s.next();
+	int ar[]=ob.stringtoIntArray(st);
+        ob.sort(ar);
+        ob.print(ar);
+     }
+}
+ 		
+
